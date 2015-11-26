0e0cbe3b653867f636168418d50eb9c91844b999
fehu <lord.fehu@gmail.com>
2015-11-26 00:05:06 -0600

    added SolveCubeImage app;

--- nxt-gl/src/main/scala/feh/tec/nxt/run/ShowCubeImage.scala
+++ nxt-gl/src/main/scala/feh/tec/nxt/run/ShowCubeImage.scala
@@ -1,20 +1,51 @@
 package feh.tec.nxt.run
 
 import feh.tec.nxt.{CreateRubikInstance, RubikCubeImageFromFile}
+import feh.tec.rubik.solve.{RubikCubeHeuristics, RubikCube_A_*}
+import feh.tec.rubik.{RubikCubeInstance, RubikCube}
+import feh.tec.rubik.RubikCube.SideName
 import feh.tec.rubik.ogl.run.RubikCubeTestGLDefault
 import feh.util.Path
 import feh.util.file._
 
-object ShowCubeImage extends RubikCubeTestGLDefault{
+trait WithCubeImage extends RubikCubeTestGLDefault{
+  lazy val filePath = Path(args.head, File.separatorChar)
+  lazy val file = filePath.file
 
-  val filePath = Path(args.head, File.separatorChar)
-  val file = filePath.file
-
-  val img = RubikCubeImageFromFile(file).map(_._2)
+  lazy val img = RubikCubeImageFromFile(file).map(_._2)
 
   implicit def sidesMap = SidesMaps.default
+  def mkInitialCube = CreateRubikInstance(img, None, filePath.splittedName._1)
+}
 
-  val rubik = CreateRubikInstance(img, None, filePath.splittedName._1)
+
+object ShowCubeImage extends WithCubeImage{
+
+
+  val rubik = mkInitialCube
 
   run()
+}
+
+
+object SolveCubeImage extends WithCubeImage{
+
+  val initial = CreateRubikInstance(img, None, filePath.splittedName._1)
+  val rubik = new RubikCubeInstance.MutableContainer(initial)
+
+
+  val solver = new RubikCube_A_*.WithTricks[SideName](RubikCubeHeuristics.SomeTricks.Stage1)
+  solver.DEBUG = true
+
+  val res = solver.search(initial)
+
+  println(res)
+  val solution = res._1.get
+
+  val sParents = solver.listParents(solution)
+
+  sParents.foreach(println)
+
+
+
 }
